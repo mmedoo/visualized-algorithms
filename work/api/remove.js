c2925e2403d14634f414ef8dbc0e03943d64e38b
mmedoo <mmedoo.active@gmail.com>
2024-02-03 21:48:52 +0200

    Delete work directory

--- work/api/remove.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("DOMContentLoaded",addjs);
-function addjs() {
-  const divs = document.querySelectorAll('div');
-  if (divs[divs.length-1].style[2] == 'z-index') {
-      divs[divs.length-1].remove();
-      console.log('beautifully done!');
-  }
-}
