d8ed431629165842b6044eb431184e7b79b31671
mmedoo <mmedoo.active@gmail.com>
2024-08-02 02:10:52 +0300

    feat: Increase number of particles in background animation

--- home/src/components/sub_components/p5_back/background.js
+++ home/src/components/sub_components/p5_back/background.js
@@ -4,7 +4,7 @@ import p5 from 'p5';
 
 
 
-const particlesNo = 70;
+const particlesNo = 100;
 
 const mouseCircum = 120;
 
@@ -14,50 +14,70 @@ var bgColor = window.matchMedia("(prefers-color-scheme: dark)").matches ? 30 : 2
 const basicSketch = (p) => {
 
 	function Particle() {
+		
 		this.x = p.random(0 + window.innerWidth);
+		
 		this.y = p.random(0 + window.innerHeight);
 	}
+	
 
 	function Pool() {
+		
 		this.particles = [];
+		
 		for (let i = 1; i <= particlesNo; i++) {
 			this.particles.push(new Particle());
 		}
 	}
+	
 
 	var sects;
+	
 
-	function gradientLine(x1, y1, x2, y2, color1, color2) {
+	function gradientLine(x1, y1, x2, y2,color2,color3) {
 
 		var grad = p.drawingContext.createLinearGradient(x1, y1, x2, y2);
 
-		grad.addColorStop(0.9, color1);
-		grad.addColorStop(0, color2);
+		grad.addColorStop(0.8, "transparent");
+		grad.addColorStop(0.5, color2);
+		grad.addColorStop(0, color3);
 
 		p.strokeWeight(1)
 		p.drawingContext.strokeStyle = grad;
 
 		p.line(x1, y1, x2, y2);
 	}
+	
 
 	Particle.prototype.connectMouse = function (mouseX, mouseY) {
+		
 		if (p.dist(mouseX, mouseY, this.x, this.y) > mouseCircum)
 			return;
 
-		gradientLine(this.x, this.y, mouseX, mouseY, 'transparent', 'rgb(230,230,230)');
+		p.stroke(255)
+		if (bgColor > 200){
+
+			gradientLine(this.x, this.y, mouseX, mouseY, 'rgba(150,150,150,0.5)', "rgb(200,200,200)");
+		} else {
+			gradientLine(this.x, this.y, mouseX, mouseY, `rgba(150,150,150,0.5)`, "rgb(55,55,55)");
+		}
+
 	}
+	
 
 	Pool.prototype.connectMouse = function (mouseX, mouseY) {
 		for (const p of this.particles) {
 			p.connectMouse(mouseX, mouseY);
 		}
 	}
+	
 
 	p.setup = () => {
 		p.createCanvas(window.innerWidth, window.innerHeight);
 		p.background(bgColor);
 		sects = new Pool(particlesNo);
 	};
+	
 
 	p.draw = () => {
 		p.background(bgColor);
@@ -72,12 +92,49 @@ const basicSketch = (p) => {
 
 		sects.connectMouse(p.mouseX, p.mouseY);
 	};
+	
+	
 	p.windowResized = () => {
 		p.resizeCanvas(window.innerWidth, window.innerHeight);
 		p.background(bgColor);
 		sects = new Pool(particlesNo);
 	};
 };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
